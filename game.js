44f1bde7f744ed0f78cc833dff15e14774eb3212
CairX <lazycairx@gmail.com>
2015-10-20 11:46:43 +0200

    Prevent overshooting to the left and right.

--- game.js
+++ game.js
@@ -2,38 +2,37 @@
 
 "use strict";
 
-var size = 32;
+var settings = {
+	size: 48,
+	width: 7,
+	height: 9
+};
+
+var colors = ["green", "yellow", "orange", "red", "purple", "blue"];
 
 var getRandom = function(min, max) {
 	return Math.floor(Math.random() * (max - min)) + min;
 };
 
 var canvas = document.getElementById("canvas");
-canvas.width = size * 7;
-canvas.height = size * 9;
+canvas.width = settings.size * settings.width;
+canvas.height = settings.size * settings.height;
 
 var context = canvas.getContext("2d");
-
-
-context.fillStyle = "green";
-context.fillRect(0, 0, 10, 10);
-
 
 function Combiner(color, x, y) {
 	this.x = x;
 	this.y = y;
 	this.color = color;
+	this.size = settings.size;
 
 	this.draw = function() {
 		context.fillStyle = this.color;
-		context.fillRect((this.x * size), (this.y * size), size, size);
+		context.fillRect((this.x * this.size), (this.y * this.size), this.size, this.size);
 	};
 }
 
-var colors = ["green", "yellow", "orange", "red", "purple", "blue"];
-
 var combiner = new Combiner("green", 0, 0);
-
 var combiners = [];
 // combiners.add(combiner);
 
@@ -68,19 +67,30 @@ window.addEventListener("keypress", function(e) {
 	// console.log(combiner);
 	switch (e.key) {
 		case "ArrowDown":
-			combiner.y = 8 - getRowHeight(combiner.x); // (canvas.height - size) - (combiners.length * size);
-			// combiner.y = 32;
-			combiners.push(combiner);
-			var color = colors[getRandom(0, colors.length)];
-			combiner = new Combiner(color, combiner.x, 0);
+			var height = getRowHeight(combiner.x);
+			if (height < 7) {
+				combiner.y = 8 - height; // (canvas.height - size) - (combiners.length * size);
+				// combiner.y = 32;
+				combiners.push(combiner);
+				var color = colors[getRandom(0, colors.length)];
+				combiner = new Combiner(color, combiner.x, 0);
+			} else {
+				// TODO: Game over
+			}
 			e.preventDefault();
 			break;
+
 		case "ArrowLeft":
-			combiner.x -= 1;
+			if (combiner.x > 0) {
+				combiner.x -= 1;
+			}
 			e.preventDefault();
 			break;
+
 		case "ArrowRight":
-			combiner.x += 1;
+			if (combiner.x + 1 < settings.width) {
+				combiner.x += 1;
+			}
 			e.preventDefault();
 			break;
 	}
