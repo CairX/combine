432ddc61209e3c01899f2470407de7a03746df3c
CairX <lazycairx@gmail.com>
2015-11-05 14:33:14 +0100

    Display level tree and what level is currently unlocked.

--- game.js
+++ game.js
@@ -38,7 +38,7 @@ var Game = (function() {
 
 	self.init = function() {
 		canvas = document.getElementById("canvas");
-		canvas.width = settings.size * settings.width;
+		canvas.width = settings.size * settings.width + 100;
 		canvas.height = settings.size * settings.height;
 		context = canvas.getContext("2d");
 
@@ -65,12 +65,13 @@ var Levels = (function() {
 		"#9c27b0", // "purple",
 		"#3f51b5", // "blue",
 		"#2196f3", // "lightblue",
-		"#000000", // "black",
+		"#333333", // "black",
 		"#FFFFFF"  // "white"
 	];
 	var min = 0;
 	var max = (colors.length - 1);
 	var unlocked = 1;
+	var size = settings.size / 2;
 
 	var self = {};
 	self.getColor = function(level) {
@@ -94,8 +95,42 @@ var Levels = (function() {
 		return Math.floor(Math.random() * ((unlocked + 1) - min)) + min;
 	};
 
+	self.draw = function(context) {
+		for (var i = 0; i < colors.length; i++) {
+			context.fillStyle = colors[i];
+			var x = 8 * settings.size;
+			var y = (settings.height * settings.size) - (i * size + size);
+
+			var radians = size / 2;
+			context.beginPath();
+			context.arc(x + radians, y + radians, radians, 0, (Math.PI * 2), true);
+			context.closePath();
+			context.fill();
+
+			var lineWidth = 2;
+			context.strokeStyle = "rgba(0, 0, 0, 0.33)";
+			context.lineWidth = lineWidth;
+			context.beginPath();
+			context.arc(x + radians, y + radians, radians - (lineWidth / 2), 0, (Math.PI * 2), true);
+			context.stroke();
+
+			if (i == unlocked) {
+				context.fillStyle = "rgba(0, 0, 0, 0.75)";
+				var arrowSize = radians / 2;
+				context.beginPath();
+				x = x + size + 3;
+				y = y + (size - (arrowSize * 2)) / 2;
+				context.moveTo(x + 0, y + arrowSize); // Middle
+				context.lineTo(x + arrowSize, y + arrowSize * 2); // Bottom
+				context.lineTo(x + arrowSize, y + 0); // Top
+				context.fill();
+			}
+		}
+	};
+
 	return self;
 })();
+Game.add(Levels);
 
 
 /* --------------------------------- */
